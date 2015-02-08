b02cfd2e7c4f7f4afa6426f39d86637befe8e01a
Cole <ricesarmagarfelos@gmail.com>
2015-02-07 16:44:57 -0800

    Initial Commit

---
+++ src/org/usfirst/frc/team3070/robot/Robot.java
@@ -0,0 +1,82 @@
+
+package org.usfirst.frc.team3070.robot;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+	
+	CANTalon lift1, lift2;
+	Joystick xbox;
+	DigitalInput upperLimit, lowerLimit, toteLimit;
+	TestLift lift;
+	
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+    	lift1 = new CANTalon(8);
+    	lift2 = new CANTalon(8);
+    	
+    	xbox = new Joystick(1);
+    	
+    	upperLimit = new DigitalInput(1);
+    	lowerLimit = new DigitalInput(2);
+    	toteLimit = new DigitalInput(3);
+    	
+    	lift = new TestLift(lift1, lift2, upperLimit, lowerLimit, toteLimit, xbox);
+
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+    	lift1.set(xbox.getRawAxis(1));
+    	lift2.set(-xbox.getRawAxis(1));
+    	
+    	// for testing if limit switches are working
+    	if (upperLimit.get()) {
+    		System.out.println("At Top");
+    	}
+    	
+    	if (lowerLimit.get()) {
+    		System.out.println("At Bottom");
+    	}
+    	
+    	if (toteLimit.get()) {
+    		System.out.println("Tote");
+    	}
+    	
+        // lift.periodic();
+    }
+    
+    public void disabledInit() {
+    	lift.stopPeriodic();
+    }
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    
+    }
+    
+}
---
+++ src/org/usfirst/frc/team3070/robot/TestLift.java
@@ -0,0 +1,154 @@
+package org.usfirst.frc.team3070.robot;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class TestLift {
+
+	interface LiftState {
+		public LiftState check();
+	}
+
+	private static final double LIFT_SPEED = .6;
+	
+	static CANTalon motor1, motor2;
+	static Joystick xbox;
+	static DigitalInput lower, upper, tote;
+	static boolean atTop, atBottom, readyForNextTote;
+	static int toteCount;
+	LiftState state;
+
+	public TestLift(CANTalon m1, CANTalon m2, DigitalInput u, DigitalInput l,
+			DigitalInput t, Joystick x) {
+		motor1 = m1;
+		motor2 = m2;
+		lower = l;
+		upper = u;
+		tote = t;
+		xbox = x;
+		atTop = false;
+		atBottom = false;
+		toteCount = 0;
+		
+		state = LiftStates.Stopped;
+	}
+
+	enum LiftStates implements LiftState {
+		Stopped {
+			@Override
+			public LiftState check() {
+				if (!atTop && xbox.getRawButton(1)) {
+					return StartLiftUp;
+				}
+
+				if (!atBottom && xbox.getRawButton(2)) {
+					return StartLiftDown;
+				}
+
+				return Stopped;
+			}
+		},
+		StartLiftUp {
+			@Override
+			public LiftState check() {
+				lift(LIFT_SPEED);
+				atBottom = false;
+				return LiftingUp;
+			}
+		},
+		LiftingUp {
+			@Override
+			public LiftState check() {
+				if (upper.get()) {
+					atTop = true;
+					return Stopping;
+				}
+				
+				if (tote.get()) {
+					return WaitForRelease;
+				}
+				
+				if (!xbox.getRawButton(1)) {
+					return Stopping;
+				}
+				
+				return LiftingUp;
+			}
+		},
+		WaitForRelease {
+			@Override
+			public LiftState check() {
+				if (tote.get()) {
+					lift(.25);
+				} else {
+					lift(0);
+				}
+				if (!xbox.getRawButton(1)) {
+					toteCount++;
+					return Stopping;
+				}
+				
+				return WaitForRelease;
+			}
+		},
+		StartLiftDown {
+			@Override
+			public LiftState check() {
+				lift(-LIFT_SPEED);
+				atTop = false;
+				return LiftingDown;
+			}
+		},
+		LiftingDown {
+			@Override
+			public LiftState check() {
+				if (lower.get() && toteCount == 1) {
+					atBottom = true;
+					return Stopping;
+				}
+				
+				if (lower.get()) {
+					return MoveDownPastSwitch;
+				}
+				
+				if (!xbox.getRawButton(2))
+					return Stopping;
+				return LiftingDown;
+			}
+		},
+		MoveDownPastSwitch {
+			@Override
+			public LiftState check() {
+				if (lower.get()) {
+					lift(-LIFT_SPEED);
+				} else {
+					toteCount--;
+					return LiftingDown;
+				}
+				return MoveDownPastSwitch;
+			}
+		},
+		Stopping {
+			@Override
+			public LiftState check() {
+				lift(0);
+				return Stopped;
+			}
+		}
+	}
+	
+	public void periodic() {
+		state = state.check();
+	}
+	
+	public void stopPeriodic() {
+		lift(0);
+	}
+	
+	private static void lift(double speed) {
+		motor1.set(speed);
+		motor2.set(-speed);
+	}
+
+}
